a53824a5955864a8f677aea190b908d42ab0960b
Tom <airbreather7@gmail.com>
2026-08-11 22:39:54 +0800

    Wire up the CSV generator

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,11 @@
 import { fetchCountries, transformCountries } from './api/country/index.js';
-import { writeHtml } from './generators/index.js';
+import { writeHtml, writeCSV } from './generators/index.js';
 
 const response = await fetchCountries({ query: { region: 'Europe' } });
 const countries = transformCountries(response);
 
 const htmlPath = await writeHtml(countries, process.env.OUTPUT_DIR);
 console.log(`Wrote ${htmlPath}`);
+
+const csvPath = await writeCSV(countries, process.env.OUTPUT_DIR);
+console.log(`Wrote ${csvPath}`);
